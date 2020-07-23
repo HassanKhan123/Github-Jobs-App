cf41ff2fdde408416923dd5488753203c3c53633
HassanKhan123 <hk2602809@gmail.com>
2020-07-23 22:00:56 +0500

    basic pagination added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import useFetchJobs from "./useFetchJobs";
 import { Container } from "react-bootstrap";
-import Job from './Job'
+import Job from "./Job";
+import Pagination from "./JobsPagination";
 
 function App() {
-  const [params,setParams] = useState({})
-  const [page,setPage] = useState(1)
-  const { jobs, loading, error } = useFetchJobs(params,page);
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
+  const { jobs, loading, error } = useFetchJobs(params, page);
   return (
     <Container className="my-4">
       <h1 className="mb-4">Github Jobs</h1>
+      <Pagination page={page} setPage={setPage} hasNextPage={true}/>
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error. Try Refreshing.</h1>}
-      {jobs.map(job => <Job key={job.id} job={job}/>)}
+      {jobs.map((job) => (
+        <Job key={job.id} job={job} />
+      ))}
+      <Pagination page={page} setPage={setPage} hasNextPage={true}/>
     </Container>
   );
 }
--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -5,6 +5,7 @@ const ACTIONS = {
   MAKE_REQUEST: "make-request",
   GET_DATA: "get-data",
   ERROR: "error",
+  UPDATE_HAS_NEXT_PAGE: "update-has-next-page",
 };
 const BASE_URL =
   "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json";
@@ -21,6 +22,12 @@ function reducer(state, action) {
         loading: false,
         error: action.payload.error,
         jobs: [],
+      };
+
+    case ACTIONS.UPDATE_HAS_NEXT_PAGE:
+      return {
+        ...state,
+        hasNextPage: action.payload.hasNextPage,
       };
 
     default:
